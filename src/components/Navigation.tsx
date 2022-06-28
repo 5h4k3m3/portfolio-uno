433c4746c7dae074df82b4ff1cec8f8d66977fcb
SalmonFillet <sy4.l3ucas@gmail.com>
2022-06-28 23:54:44 +0900

    🚧 nav section

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,11 +2,18 @@ import "../main.css";
 
 export const Navigation = () => {
   return (
-    <ul>
-      <li>#About</li>
-      <li>#Works</li>
-      <li>#Skills</li>
-      <li>#Contact</li>
-    </ul>
+    <div className="navigationCard mt-28 ml-40 flex">
+      <ul className="list-none font-zenMaru text-4xl font-bold text-mainText items-center justify-center m-10">
+        <li className="cursor-pointer p-4">
+          <a># About</a>
+        </li>
+        <li className="cursor-pointer p-4">
+          <a># Works</a>
+        </li>
+        <li className="cursor-pointer p-4">
+          <a># Skills</a>
+        </li>
+      </ul>
+    </div>
   );
 };
