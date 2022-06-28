5a0ce2a9a9be3bdd459cbee4f6a8f8dae9d671b3
SalmonFillet <sy4.l3ucas@gmail.com>
2022-06-28 22:33:23 +0900

    ✨ title, profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,19 @@
 import "../main.css";
+import ProfileImg from "../images/profile.jpg";
 
 export const Profile = () => {
-  return <></>;
+  return (
+    <div className="flex">
+      <div className="profileIconWrapper w-64 h-auto mt-4 ml-20">
+        <img
+          src={ProfileImg}
+          className="rounded-full items-center justify-center p-4"
+        />
+      </div>
+      <div className="nameWrapper font-zenMaru font-bold px-10 pt-20 pb-10">
+        <h3 className="text-5xl text-mainText pb-4">しゃけのきりみ</h3>
+        <h3 className="text-4xl text-subText ml-2">Salmon Fillet</h3>
+      </div>
+    </div>
+  );
 };
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,9 +2,9 @@ import "../main.css";
 
 export const Title = () => {
   return (
-    <>
-      <h1>Salmon's</h1>
-      <h2>#Shake Me?</h2>
-    </>
+    <div className="font-zenMaru font-bold px-20 pt-20 pb-10">
+      <h1 className="text-8xl text-mainText mb-8">Salmon's</h1>
+      <h2 className="text-4xl text-subText">#Shake Me?</h2>
+    </div>
   );
 };
