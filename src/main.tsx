27727689dde2554f84197aff850cb4bdede553c5
SalmonFillet <sy4.l3ucas@gmail.com>
2022-06-28 23:53:17 +0900

    ✨ impl mouse stalker

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,21 @@ import { Footer } from "./pages/Footer";
 import { Skills } from "./pages/Skills";
 import { Works } from "./pages/Works";
 
+// mouse cursor
+const mouse = document.querySelector(".mouse") as HTMLElement;
+const circle = document.querySelector(".circle") as HTMLElement;
+
+document.addEventListener("mousemove", (e) => {
+  if (mouse === null || circle === null) return;
+  const setProperty = `translate(${e.pageX}px, ${e.pageY}px`;
+  mouse.style.transform = setProperty;
+  circle.style.transform = setProperty;
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div className="bg-background w-screen h-screen absolute top-0 left-0">
-      <div className="mainCard select-none m-20">
+    <div className="background">
+      <div className="mainCard select-none m-20 h-full">
         <Home />
         <Skills />
         <Works />
