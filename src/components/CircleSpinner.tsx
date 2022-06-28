cc651fd86668fc15503dbddf34b97931b6a9657e
SalmonFillet <sy4.l3ucas@gmail.com>
2022-06-28 20:27:42 +0900

    ✨ impl circle spinner

---
+++ src/components/CircleSpinner.tsx
@@ -0,0 +1,10 @@
+import "./CircleSpinner.css";
+
+export const CircleSpinner = () => {
+  return (
+    <div className="fulfilling-bouncing-circle-spinner">
+      <div className="circle"></div>
+      <div className="orbit"></div>
+    </div>
+  );
+};
