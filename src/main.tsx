9d5a898eb4dc949b0bb404cb805b0dad17191fbc
SalmonFillet <sy4.l3ucas@gmail.com>
2022-06-29 22:59:01 +0900

    🔥 remove mouse stalker

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,10 @@ import { Works } from "./pages/Works";
 
 // mouse cursor
 const mouse = document.querySelector(".mouse") as HTMLElement;
-const circle = document.querySelector(".circle") as HTMLElement;
-
 document.addEventListener("mousemove", (e) => {
-  if (mouse === null || circle === null) return;
+  if (mouse === null) return;
   const setProperty = `translate(${e.pageX}px, ${e.pageY}px`;
   mouse.style.transform = setProperty;
-  circle.style.transform = setProperty;
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
