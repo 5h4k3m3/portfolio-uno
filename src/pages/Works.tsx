1c9c316838aa3bda727e35b9fb926616c526d2da
SalmonFillet <sy4.l3ucas@gmail.com>
2022-07-02 22:02:45 +0900

    🚧 wip works

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -3,14 +3,14 @@ import { SubTitle } from "../components/atoms/SubTitle";
 
 export const Works = () => {
   return (
-    <>
+    <div className="m-20 absolute">
       <SubTitle># Works</SubTitle>
-      <div className="flex h-1/3 justify-between px-8">
+      <div className="flex justify-between px-8">
         <SkillCard title="Portfolio" comment="#beginner" />
         <SkillCard title="Discord Bot" comment="#beginner" />
         <SkillCard title="WIP" comment="" />
         <SkillCard title="WIP" comment="" />
       </div>
-    </>
+    </div>
   );
 };
