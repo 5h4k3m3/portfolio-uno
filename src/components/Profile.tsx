6dc3bf419cb296b7bdd4b87f2f9eb29cbaa31129
SalmonFillet <sy4.l3ucas@gmail.com>
2022-07-12 22:30:37 +0900

    💄 fix name font size

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -7,7 +7,7 @@ export const Profile = () => {
     <div className="flex flex-col items-center justify-center mt-16 mx-4">
       <RoundIcon imgSrc={ProfileImg} linkTo={""} />
       <div className="font-zenMaru font-bold px-4 py-8">
-        <p className="text-4xl text-mainText pb-2">しゃけのきりみ</p>
+        <p className="text-3xl text-mainText pb-2">しゃけのきりみ</p>
         <p className="text-2xl text-subText ml-2">Salmon Fillet</p>
       </div>
       <SocialIcons />
