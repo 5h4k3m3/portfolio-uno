9a0deab7150e502c02af165a6b41427a8bcdb210
SalmonFillet <sy4.l3ucas@gmail.com>
2022-07-15 22:42:03 +0900

    ✨ add sns links

--- src/components/molecules/SocialIcons.tsx
+++ src/components/molecules/SocialIcons.tsx
@@ -8,9 +8,9 @@ import QiitaImg from "../../images/qiita.png";
 export const SocialIcons: FC = () => {
   return (
     <div className="flex items-center justify-center space-x-10">
-      <RoundIcon imgSrc={GithubImg} linkTo={"#"} />
-      <RoundIcon imgSrc={TwitterImg} linkTo={"#"} />
-      <RoundIcon imgSrc={QiitaImg} linkTo={"#"} />
+      <RoundIcon imgSrc={GithubImg} linkTo={"https://github.com/5h4k3m3"} />
+      <RoundIcon imgSrc={TwitterImg} linkTo={"https://twitter.com/5h4k3m3"} />
+      <RoundIcon imgSrc={QiitaImg} linkTo={"https://qiita.com/5h4k3m3"} />
     </div>
   );
 };
